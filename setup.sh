11127a92a7d01093afc5d1e6f5defac878025015
Aetf <aetf@unlimited-code.works>
2021-07-30 15:25:44 -0400

    Simplify

--- setup.sh
+++ setup.sh
@@ -70,23 +70,8 @@ curl -s https://api.github.com/repos/Nukesor/pueue/releases/latest |
 curl -s https://api.github.com/repos/dalance/procs/releases/latest |
     grep -oP "browser_download_url.*\Khttp.*x86_64-lnx.zip" |
     xargs -n 1 curl -JL -o install.zip &&
-    unzip -d /usr/local/bin install.zip &&
+    unzip -o -d /usr/local/bin install.zip &&
     rm install.zip
-
-# mongodb on node-1
-if [[ $(hostname) == node-1* ]]; then
-    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-    sudo apt-get update
-    sudo apt-get install -y mongodb-org
-
-    vlan=$(basename $(find /sys/class/net -iname 'vlan*'))
-    ip=$(ip a show dev $vlan | rg -Po 'inet \K\d+\.\d+\.\d+\.\d+')
-
-    sed -i -E 's/(\s*bindIp:\s).*/\1'"$ip"'/g' /etc/mongod.conf
-
-    sudo systemctl enable --now mongod
-fi
 
 echo "Setting default editor to neovim"
 for exe in vi vim editor; do
@@ -110,6 +95,7 @@ fi
 echo "Setting up python"
 CONDA_PREFIX=/opt/miniconda3
 curl -JOL 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+rm -rf "$CONDA_PREFIX"
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_PREFIX
 rm Miniconda3-latest-Linux-x86_64.sh
 cat <<CONDARC > $CONDA_PREFIX/condarc
@@ -120,7 +106,10 @@ channels:
   - conda-forge
   - defaults
 CONDARC
-cat <<EOF >> /etc/zsh/zshenv
+
+if !grep 'conda_setup' /etc/zsh/zshenv; then
+
+    cat <<EOF >> /etc/zsh/zshenv
 __conda_setup="\$('/opt/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ \$? -eq 0 ]; then
     eval "\$__conda_setup"
@@ -132,9 +121,11 @@ else
     fi
 fi
 EOF
+
+fi
 ln -sf $CONDA_PREFIX/etc/profile.d/conda.sh /etc/profile.d
 $CONDA_PREFIX/bin/conda install --yes pip ipython jupyter jupyterlab matplotlib cython
-$CONDA_PREFIX/bin/conda install --yes pytorch=1.5.0 torchvision cudatoolkit=10.2 -c pytorch
+#$CONDA_PREFIX/bin/conda install --yes pytorch=1.5.0 torchvision cudatoolkit=10.2 -c pytorch
 # make sure everyone can install
 chgrp -R $PROJ_GROUP /opt/miniconda3
 chmod -R g+w /opt/miniconda3
