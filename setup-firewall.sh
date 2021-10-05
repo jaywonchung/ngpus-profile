e56a7e5201201e51345960eb88feba54ac434426
Aetf <aetf@unlimited-code.works>
2021-10-05 11:17:34 -0400

    Fix firewall installation

--- setup-firewall.sh
+++ setup-firewall.sh
@@ -13,8 +13,27 @@ if [[ $EUID -ne 0 ]]; then
     exec sudo /bin/bash "$0" "$@"
 fi
 
+# update repo
+echo "Updating profile repo"
+if [[ -d /local/repository ]]; then
+    cd /local/repository
+    chgrp -R $PROJ_GROUP /local/repository
+    chmod -R g+w /local/repository
+    git checkout master
+
+    changed=false
+    git remote update && git status -uno | grep -q 'Profile repo branch is behind' && changed=true
+    if $changed; then
+        git pull
+        echo "Updated successfully, reexec setup.sh"
+        exec /local/repository/setup.sh
+    else
+        echo "Up-to-date"
+    fi
+fi
+
 # Setup firewall
 echo "Setup firewall"
-apt-get install -y firewalld
+apt-get update && apt-get install -y firewalld
 firewall-cmd --zone=trusted --add-source=192.168.0.0/16
 firewall-cmd --runtime-to-permanent
