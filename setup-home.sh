38564f60cba9ba52567d190537c5a8c4ab833133
Jae-Won Chung <jwnchung@umich.edu>
2022-07-12 09:40:18 -0400

    Fix errors in Chameleon Cloud

--- setup-home.sh
+++ setup-home.sh
@@ -35,11 +35,6 @@ chsh -s $(which zsh) $TARGET_USER
 # add user to docker group
 usermod -aG docker $TARGET_USER
 
-# fix mounting point
-if [[ -d $TARGET_HOME/my_mounting_point ]]; then
-  umount $TARGET_HOME/my_mounting_point
-fi
-
 # fix permission
 echo "Fixing permission"
 chown -R $TARGET_USER:$TARGET_GROUP $TARGET_HOME
